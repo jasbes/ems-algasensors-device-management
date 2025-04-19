80d9c434c891566583fa842c358344a028b6773f
Jefferson <jefferson_uk@hotmail.com>
2025-04-19 21:57:57 +0200

    - Implement endpoint to find sensor by id

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -2,16 +2,27 @@ package com.algaworks.algasensors.device.management.api.controller;
 
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
 import com.algaworks.algasensors.device.management.api.model.SensorOutput;
+import com.algaworks.algasensors.device.management.domain.model.SensorId;
 import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors")
 @RequiredArgsConstructor
 public class SensorController {
     private final SensorRepository sensorRepository;
+
+    @GetMapping("{sensorId}")
+    public SensorOutput getOne(@PathVariable TSID sensorId) {
+        return SensorOutput.fromEntity(
+                sensorRepository.findById(new SensorId(sensorId))
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND))
+        );
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
